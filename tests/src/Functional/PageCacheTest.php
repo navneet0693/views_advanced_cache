879fadc17d244e7cd0a6eab3fc83199b09cf6e47
Poindexter, Malcolm <Malcolm.Poindexter@umusic.com>
2018-07-03 18:33:49 -0700

    ASF8-655 working cache metadata test

---
+++ tests/src/Functional/PageCacheTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Drupal\Tests\views_advanced_cache\Functional;
+
+
+class PageCacheTest
+{
+
+  public function _testPageCache() {
+    // TODO
+    $this->markTestIncomplete();
+  }
+
+}
--- tests/src/Functional/ViewsCacheMetadataTest.php
+++ tests/src/Functional/ViewsCacheMetadataTest.php
@@ -25,10 +25,8 @@ class ViewsCacheMetadataTest extends BrowserTestBase {
     $display_name = 'block_test';
 
     // Render the view with the default cache tags,
-
     $view = Views::getView($view_name);
-    $view->setDisplay($display_name);
-    $element = $this->render($view);
+    $element = $this->render($view, $display_name);
     // And verify that the default node entity_type list cache tag is present.
     $this->assertTrue(in_array('node_list', $element['#cache']['tags']), 'The view has the node_list cache tag.');
 
@@ -44,20 +42,51 @@ class ViewsCacheMetadataTest extends BrowserTestBase {
 
     // Now re-render the view element and check its cache tags.
     $view = Views::getView($view_name);
-    $view->setDisplay($display_name);
-    $element = $this->render($view);
-    $this->assertFalse(in_array('node_list', $element['#cache']['tags']), 'The view no longer has the node_list cache tag.');
+    $element = $this->render($view, $display_name);
     $this->assertTrue(in_array('vact:node_list:test', $element['#cache']['tags']), 'The view has the vact:node_list:bundle cache tag.');
+    $this->assertFalse(in_array('node_list', $element['#cache']['tags']), 'The view no longer has the node_list cache tag.');
+
+    // Test adding / removing cache contexts.
+    $query_args_context = in_array('url.query_args:page', $element['#cache']['contexts']) ? 'url.query_args:page' : 'url.query_args';
+    $view = Views::getView($view_name);
+    $this->assertTrue(in_array($query_args_context, $element['#cache']['contexts']), 'The view has the url.query_args cache context.');
+    $cacheOptions = $this->getCacheOptions($view->storage, $display_name);
+    $cacheOptions = NestedArray::mergeDeep($cacheOptions, [
+      'cache_contexts' => ['url.query_args:test'],
+      'cache_contexts_exclude' => [$query_args_context],
+    ]);
+    $this->setCacheOptions($view->storage, $display_name, $cacheOptions);
+    $view->storage->save();
+
+    $view = Views::getView($view_name);
+    $element = $this->render($view, $display_name);
+    $this->assertTrue(in_array('url.query_args:test', $element['#cache']['contexts']), 'The view has the url.query_args:test cache context.');
+    $this->assertFalse(in_array($query_args_context, $element['#cache']['contexts']), 'The view no longer has the url.query_args cache context.');
+
+    // Now let's cache the view for 600 seconds.
+    $view = Views::getView($view_name);
+    $cacheOptions = $this->getCacheOptions($view->storage, $display_name);
+    $cacheOptions = NestedArray::mergeDeep($cacheOptions, [
+      'results_lifespan' => 600,
+      'output_lifespan' => 600,
+    ]);
+    $this->setCacheOptions($view->storage, $display_name, $cacheOptions);
+    $view->storage->save();
+
+    $view = Views::getView($view_name);
+    $element = $this->render($view, $display_name);
+    $this->assertEquals(600, $element['#cache']['max-age'], 'The view is cached for 600 seconds.');
   }
 
   /**
    * @param $view
    * @return array
    */
-  protected function render(ViewExecutable $view) {
+  protected function render(ViewExecutable $view, $display_name) {
+    $view->setDisplay($display_name);
     $view->preExecute();
-    $view->execute();
-    return $view->buildRenderable();
+    $view->execute($display_name);
+    return $view->buildRenderable($display_name);
   }
 
   /**
@@ -81,11 +110,5 @@ class ViewsCacheMetadataTest extends BrowserTestBase {
     // TODO
     $this->markTestIncomplete();
   }
-
-  public function _testPageCache() {
-    // TODO
-    $this->markTestIncomplete();
-  }
-
 
 }
